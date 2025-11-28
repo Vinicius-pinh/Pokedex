fd97d73c09c74ad2c6ea7e83593528e1c5210309
Vinicius Pinheiro <73889866+Vinicius-pinh@users.noreply.github.com>
2025-11-28 19:57:27 -0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Typebar from './components/TypeBar'
+
+function App() {
+
+  return (
+    <>
+      <label>Pokedex</label>
+      <Typebar />
+    </>
+  )
+}
+
+export default App
