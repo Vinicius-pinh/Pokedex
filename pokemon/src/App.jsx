710ab202a08d97907e2ca17a7a7eb00f8f27a295
Vinicius Pinheiro <73889866+Vinicius-pinh@users.noreply.github.com>
2025-11-28 21:22:45 -0300

    update prof

--- pokemon/src/App.jsx
+++ pokemon/src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import TypeBar from './componentes/typeBar'
-import PokemonContainer from './componentes/pokemonContainer'
+import Typebar from './components/TypeBar'
 
 function App() {
 
   return (
     <>
-    <label>Pokedex</label>
-    <TypeBar/>
-    <PokemonContainer/>
+      <label>Pokedex</label>
+      <Typebar />
     </>
   )
 }
